479ef5e0d8f70a2fc921bed850606d7cc0b858e9
Hannes <h@nnes.dev>
2022-08-25 17:54:02 +0100

    Split docker compose generation script into functions

--- generate-docker-compose.py
+++ generate-docker-compose.py
@@ -1,18 +1,26 @@
 import json
 from pathlib import Path
+from typing import List, Any, Dict
+
+
+def read_language_list(languages_file: Path) -> List[str]:
+    with open(languages_file, "r") as f:
+        return f.read().splitlines()
 
 
 def generate_service(language: str):
     return {"build": language, "volumes": [f"$PWD/results/{language}:/results"]}
 
 
-if __name__ == "__main__":
-    with open(Path(".") / "languages.txt", "r") as f:
-        languages = f.read().splitlines()
-
+def generate_docker_compose(languages: List[str]) -> Dict[str, Any]:
     services = {language: generate_service(language) for language in languages}
+    return {"version": "3.9", "services": services}
 
-    docker_compose = {"version": "3.9", "services": services}
+
+if __name__ == "__main__":
+    languages = read_language_list(Path(".") / "languages.txt")
+
+    docker_compose = generate_docker_compose(languages)
 
     with open(Path(".") / "docker-compose.yaml", "w") as f:
         json.dump(docker_compose, f)
