399e1e9d234dbc5f96e16c7188f34b9f29343bca
Hannes <h@nnes.dev>
2022-08-30 21:08:27 +0100

    Refactor analysis script to be more automated

--- analysis/src/analysis.jl
+++ analysis/src/analysis.jl
@@ -1,46 +1,34 @@
-using Chain
+using Chain: @chain
 using DataFrames
 using CairoMakie
 using AlgebraOfGraphics
 
+read_benchmark_result(file_path) = open(f -> parse(Float32, String(read(f))), file_path, "r")
 
+read_benchmark_results(benchmarks_folder) = DataFrame(
+    (
+        language=language,
+        benchmark=benchmark_name,
+        time=read_benchmark_result(joinpath(benchmarks_folder, language, benchmark_name)),
+    ) for language in readdir(benchmarks_folder) for
+    benchmark_name in readdir(joinpath(benchmarks_folder, language))
+)
 
-function read_benchmark(language, benchmark)
-    file_path = joinpath(pwd(), "..", "results", language, benchmark)
-    time = isfile(file_path) ? open(f -> parse(Float32, String(read(f))), file_path, "r") : missing
-    return (language = language, benchmark = benchmark, time = time)
-end
-
-function read_benchmarks(languages, benchmarks)
-    return DataFrame(read_benchmark(language, benchmark) for language in languages for benchmark in benchmarks)
-end
 
 function plot_benchmarks(df)
     @chain df begin
-        data(_) * mapping(:language, :time_normalised, color = :benchmark)
-        draw(axis = (width = 225, height = 225, yscale = log10))
+        data(_) * mapping(:language, :time_normalised, color=:benchmark)
+        draw(axis=(width=225, height=225, yscale=log10))
     end
 end
 
 function main()
-    output_directory = joinpath(pwd(), "output")
+    results_folder = ARGS[1]
+    output_directory = ARGS[2]
     mkpath(output_directory)
 
-    languages = ["python", "julia", "c", "crystal", "go", "javascript", "lua", "nim", "octave", "r", "zig"]
-    benchmarks = [
-        "print_to_file",
-        "userfunc_mandelbrot",
-        "iteration_pi_sum",
-        "matrix_multiply",
-        "recursion_fibonacci",
-        "matrix_statistics",
-        "parse_integers",
-        "recursion_quicksort",
-    ]
-
     df = @chain begin
-        read_benchmarks(languages, benchmarks)
-        subset(:time => ByRow(!ismissing))
+        read_benchmark_results(results_folder)
         groupby(:benchmark)
         transform([:language, :time] => ((ls, ts) -> ts ./ ts[ls .== "julia"]) => :time_normalised)
     end
